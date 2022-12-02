601f383f16eac1046cc7b8f132d0468409e68ba5
Jared Grimes <jaredlgrimes@hotmail.com>
2022-12-02 09:22:17 -0500

    finished base site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+  h1, h2 {
+    margin: 0;
+  }
+
+  h1 {
+    font-size: 5rem;
+    color: #2D3748;
+    line-height: 5rem;
+  }
+
+  h2 {
+    font-size: 2rem;
+    color: #4A5568;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    padding-top: min(200px, 30vh);
+    padding-left: 20px;
+    gap: 8px;
+  }
+</style>
+
+<div class="container">
+  <h1>Hi, I'm Jared.</h1>
+  <h2>There's nothing here. We're working on it ✨</h2>
+</div>
